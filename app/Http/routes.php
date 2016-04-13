ad085c46738536adb316dec6fc7a2edd73505fdf
mzyil <mzyil12@gmail.com>
2016-04-13 23:11:21 +0300

    Add controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,8 @@
 Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
-
+Route::resource('etkinlik','Etkinlikler');
+Route::resource('kulup','KuluplerController');
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
